4bf47f5d7130d9ec953e733e30387e921586a8ac
Anton Österberg <anton.osterberg@gmail.com>
2017-07-01 01:31:53 +0200

    Minor improvements.

--- src/main/kotlin/com/mantono/kann/Perceptron.kt
+++ src/main/kotlin/com/mantono/kann/Perceptron.kt
@@ -3,6 +3,7 @@ package com.mantono.kann
 data class Perceptron(val weights: Array<Double>)
 {
 	constructor(numberOfWeights: Int):this(generateWeights(numberOfWeights))
+	operator fun get(i: Int): Double = weights[i]
 }
 
 fun guess(perceptron: Perceptron, inputs: Array<Float>): Int
@@ -14,21 +15,20 @@ fun guess(perceptron: Perceptron, inputs: Array<Float>): Int
 	return Math.signum(i).toInt()
 }
 
-fun correct(per: Perceptron, trainingData: Array<Float>, error: Int): Perceptron
+fun correct(perceptron: Perceptron, trainingData: Array<Float>, error: Int, learningRate: Float): Perceptron
 {
-	val adjustedWeights: Array<Double> = trainingData.asSequence()
-			.map { it * error }
-			.map(Float::toDouble)
+	val adjustedWeights: Array<Double> = trainingData.indices.asSequence()
+			.map { perceptron[it] + trainingData[it] * error * learningRate}
 			.toList()
 			.toTypedArray()
 
 	return Perceptron(adjustedWeights)
 }
 
-tailrec fun train(per: Perceptron, trainingData: Array<Float>, target: Int): Perceptron
+tailrec fun train(per: Perceptron, trainingData: Array<Float>, target: Int, learningRate: Float = 0.1f): Perceptron
 {
 	val guess: Int = guess(per, trainingData)
 	val error: Int = target - guess
-	val perceptron = correct(per, trainingData, error)
+	val perceptron = correct(per, trainingData, error, learningRate)
 	return if(error == 0) perceptron else train(perceptron, trainingData, target)
 }
