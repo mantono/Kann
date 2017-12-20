5c3c833e9d9ba17ef22333506f7350712577f1bc
Anton Österberg <anton.osterberg@gmail.com>
2017-12-20 22:49:59 +0100

    Trying to work something out, may not work out that well. Yet.

---
+++ src/main/kotlin/com/mantono/kann/NetworkStructure.kt
@@ -0,0 +1,19 @@
+package com.mantono.kann
+
+import com.mantono.kann.ga.Neuron
+
+data class NetworkStructure(private val layers: List<List<Neuron>>)
+{
+	constructor(vararg layers: Int): this(varargsToLayers(layers.toList()))
+
+	operator fun get(layer: Int, neuron: Int): Neuron = layers[layer][neuron]
+	operator fun set(layer: Int, neuron: Int): NetworkStructure
+	{
+		val index: Int = x + (y * width)
+		val previous: T? = data[index]
+		data[index] = e
+		return previous
+	}
+}
+
+private fun varargsToLayers(toList: List<Int>): List<List
--- src/main/kotlin/com/mantono/kann/ga/NeuralNetwork.kt
+++ src/main/kotlin/com/mantono/kann/ga/NeuralNetwork.kt
@@ -1,6 +1,48 @@
 package com.mantono.kann.ga
 
-data class NeuralNetwork(val inputs: Int, val outputs: Int)
+import com.mantono.kann.NetworkStructure
+
+data class NeuralNetwork(private val neurons: List<Neuron>, private val layers: List<Int>)
 {
-	fun input(inputValues: Array<Double>)
+	init
+	{
+		if(layers.sum() != neurons.size)
+			throw IllegalArgumentException("Size does not match ${layers.sum()} != ${neurons.size}")
+	}
+
+	constructor(): this(emptyList(), emptyList())
+
+	operator fun get(layer: Int, neuron: Int): Neuron
+	{
+		val index: Int = neuron + layers.take(layer).sum()
+		return neurons[index]
+	}
+
+	operator fun set(layer: Int, neuron: Int, n: Neuron): NeuralNetwork
+	{
+		val alteredLayers = layers.
+		alteredLayers[layer][neuron] = n
+		return NeuralNetwork(alteredLayers)
+	}
+
+	fun add(layer: Int, neuron: Neuron): NeuralNetwork
+	{
+		val alteredLayers: Array<Array<Neuron>> = layers.mapIndexed { index, arrayOfNeurons ->
+			when(index == layer)
+			{
+				true -> arrayOf(arrayOfNeurons + neuron)
+				false -> arrayOfNeurons
+			}
+		}
+				.toTypedArray() as Array<Array<Neuron>>
+
+		return NeuralNetwork(alteredLayers)
+	}
+
+	fun input(inputValues: Array<Double>) {}
+}
+
+private fun varargsToLayers(layers: List<Int>, init: (Int) -> Neuron): List<Neuron>
+{
+	return layers.sum()
 }
