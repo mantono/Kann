6cf4e60aa98569054b7a05901ee1ec6f739b71b6
Anton Österberg <anton.osterberg@gmail.com>
2017-12-23 21:29:34 +0100

    Better distribution.

--- src/main/kotlin/com/mantono/kann/weightGeneration.kt
+++ src/main/kotlin/com/mantono/kann/weightGeneration.kt
@@ -30,13 +30,12 @@ fun randomSeed(seed: Long, deterministic: Boolean = true): Sequence<Long> = buil
 
 fun generateSeedFrom(vararg seedInputs: Number): Long
 {
-	print("Inputs ${seedInputs.joinToString(separator = ", ")} -> ")
 	return seedInputs.asSequence()
 			.map { it.toLong() }
 			.reduceIndexed { index, accumulated, current ->
-				(accumulated + 997 * current) shl (current % index).toInt()
+				val shifts: Int = (current % index).toInt()
+				(accumulated shl shifts) xor (current * 7919)
 			}
-			.also { println("$it") }
 }
 
 
