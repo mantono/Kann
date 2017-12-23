385066db6cee06b9115e68b47975cbe9c3854fd3
Anton Österberg <anton.osterberg@gmail.com>
2017-12-23 21:23:17 +0100

    Use bitshift to left for pseudorandom seed.

--- src/main/kotlin/com/mantono/kann/weightGeneration.kt
+++ src/main/kotlin/com/mantono/kann/weightGeneration.kt
@@ -14,7 +14,6 @@ fun randomSequence(seed: Long, deterministic: Boolean = true): Sequence<Double> 
 	while(true)
 	{
 		val next: Double = rand.nextGaussian()
-		println("$seed -> $next")
 		yield(next)
 	}
 }
@@ -31,9 +30,13 @@ fun randomSeed(seed: Long, deterministic: Boolean = true): Sequence<Long> = buil
 
 fun generateSeedFrom(vararg seedInputs: Number): Long
 {
+	print("Inputs ${seedInputs.joinToString(separator = ", ")} -> ")
 	return seedInputs.asSequence()
 			.map { it.toLong() }
-			.reduce { acc, i -> 101 * (acc + 1) * (i + 1) }
+			.reduceIndexed { index, accumulated, current ->
+				(accumulated + 997 * current) shl (current % index).toInt()
+			}
+			.also { println("$it") }
 }
 
 
